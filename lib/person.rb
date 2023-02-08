0b863c4e503c1c3900e8b3123e7e0801bb415195
Joanna <astronautnemo@gmail.com>
2023-02-08 23:08:47 +0300

    completed assignment

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,21 @@
 class Person
+    # @name Setter Method
+    def name= (name)
+        @name = name
+    end
 
+    # @name Getter Method
+    def name
+        @name
+    end
+
+    # @job Setter Method
+    def job= (job)
+        @job = job
+    end
+
+    # @job Getter Method
+    def job
+        @job
+    end
 end
