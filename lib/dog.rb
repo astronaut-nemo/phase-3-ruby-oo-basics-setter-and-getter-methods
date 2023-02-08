a2db0e3265c37629c0ba01aea642716f7752125e
Joanna <astronautnemo@gmail.com>
2023-02-08 23:07:16 +0300

    completed Dog deliverable

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,21 @@
 class Dog
+    # @name Setter Method
+    def name= (name)
+        @name = name
+    end
 
+    # @name Getter Method
+    def name
+        @name
+    end
+
+    # @breed Setter Method
+    def breed= (breed)
+        @breed = breed
+    end
+
+    # @breed Getter Method
+    def breed
+        @breed
+    end
 end
